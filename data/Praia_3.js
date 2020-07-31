ca21d89270dfbb5866cd9b379888dae6693cedff
André Silva <andregeorisk@gmail.com>
2020-07-31 16:31:08 -0300

    Add files via upload

---
+++ data/Praia_3.js
@@ -0,0 +1,18 @@
+var json_Praia_3 = {
+"type": "FeatureCollection",
+"name": "Praia_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Praia": "Morrinhos" }, "geometry": { "type": "Point", "coordinates": [ -48.517302399302089, -27.183279817063845 ] } },
+{ "type": "Feature", "properties": { "Praia": "Conceição" }, "geometry": { "type": "Point", "coordinates": [ -48.491266093313399, -27.201975473299441 ] } },
+{ "type": "Feature", "properties": { "Praia": "Tainha" }, "geometry": { "type": "Point", "coordinates": [ -48.509681091479855, -27.215949509255339 ] } },
+{ "type": "Feature", "properties": { "Praia": "Zimbros" }, "geometry": { "type": "Point", "coordinates": [ -48.533047780161489, -27.180691789874025 ] } },
+{ "type": "Feature", "properties": { "Praia": "Canto Grande" }, "geometry": { "type": "Point", "coordinates": [ -48.50463662262797, -27.192513589967337 ] } },
+{ "type": "Feature", "properties": { "Praia": "Bombinhas" }, "geometry": { "type": "Point", "coordinates": [ -48.489710698412857, -27.147825887838437 ] } },
+{ "type": "Feature", "properties": { "Praia": "Bombas" }, "geometry": { "type": "Point", "coordinates": [ -48.508341998444045, -27.138858170022122 ] } },
+{ "type": "Feature", "properties": { "Praia": "Mariscal" }, "geometry": { "type": "Point", "coordinates": [ -48.498297998157668, -27.184642495481345 ] } },
+{ "type": "Feature", "properties": { "Praia": "Sepultura" }, "geometry": { "type": "Point", "coordinates": [ -48.478043778375003, -27.141331467277411 ] } },
+{ "type": "Feature", "properties": { "Praia": "Quatro Ilhas" }, "geometry": { "type": "Point", "coordinates": [ -48.483900351192155, -27.156188222382013 ] } },
+{ "type": "Feature", "properties": { "Praia": "Retiro dos Padres" }, "geometry": { "type": "Point", "coordinates": [ -48.476487932837124, -27.144658497181005 ] } }
+]
+}
